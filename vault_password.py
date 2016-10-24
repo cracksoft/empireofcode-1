0aa1f92c4158c9a8e73e0c9da90963e102b1361c
rawgni <ingwar.wirjawan@gmail.com>
2016-10-24 21:41:48 +0700

    vault password r3

--- vault_password.py
+++ vault_password.py
@@ -1,1 +1,1 @@
-golf=lambda p:len(p)>9 and all([any([x.isupper() for x in p]),any([x.islower() for x in p]),any([x.isdigit() for x in p])])
+golf=lambda p:len(p)>9 and all(any(map(t, p)) for t in (str.isupper,str.islower,str.isdigit))
