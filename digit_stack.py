3503806970c6b46e564cab2241fc0470332f32a6
rawgni <ingwar.wirjawan@gmail.com>
2016-10-24 23:34:49 +0700

    digit stack r2

--- digit_stack.py
+++ digit_stack.py
@@ -1,12 +1,11 @@
-def golf(commands):
-    k = []
-    sum = 0
-    for x in commands:
-        y = x.split()
-        if y[0] == "PUSH":
-            k.append(int(y[1]))
-        elif y[0] == "POP":
-            sum += k.pop() if len(k) > 0 else 0
-        else:
-            sum += k[len(k)-1] if len(k) > 0 else 0
-    return sum
+def golf(c,t=0):
+ k=[]
+ for x in c:
+  y=x.split()
+  if y[0]=="PUSH":
+   k.append(int(y[1]))
+  elif y[0]=="POP":
+   t+=k.pop() if len(k)>0 else 0
+  else:
+   t+=k[len(k)-1] if len(k)>0 else 0
+ return t
