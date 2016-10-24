3e854f3d61ddeae1340b4ceee297f72f4e04cd40
rawgni <ingwar.wirjawan@gmail.com>
2016-10-24 09:58:04 +0700

    digit multiplication r3

--- digit_multiplication.py
+++ digit_multiplication.py
@@ -1,12 +1,5 @@
 def golf(x):
-    t = 1
-    for i in str(x):
-        t *= max(int(i),1)
-    return t
-
-if __name__ == '__main__':
-    assert golf(123405) == 120
-    assert golf(999) == 729
-    assert golf(1000) == 1
-    assert golf(1111) == 1
-    print("Coding complete? Click 'Check' to review your tests and earn cool rewards!")
+ t = 1
+ for i in str(x):
+  t *= max(int(i),1)
+ return t
