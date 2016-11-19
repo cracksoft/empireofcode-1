b0d67e375c77a580a70f0c8e59eb80a7b2cf9a67
rawgni <ingwar.wirjawan@gmail.com>
2016-11-20 01:06:09 +0700

    index power js

---
+++ index_power.js
@@ -0,0 +1,16 @@
+"use strict";
+
+function indexPower(array, n) {
+  return array.length>n?Math.pow(array[n],n):-1;
+}
+
+var assert = require('assert');
+
+if (!global.is_checking) {
+  // These "asserts" using only for self-checking and not necessary for auto-testing
+  assert.equal(indexPower([1, 2, 3, 4], 2), 9, "Square");
+  assert.equal(indexPower([1, 3, 10, 100], 3), 1000000, "Cube");
+  assert.equal(indexPower([0, 1], 0), 1, "Zero power");
+  assert.equal(indexPower([1, 2], 3), -1, "IndexError");
+  console.log("Use 'Check' to earn sweet rewards!");
+}
