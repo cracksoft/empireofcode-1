50651d8477243259b6f57cedd9bfbabba63f0846
rawgni <ingwar.wirjawan@gmail.com>
2016-11-19 23:51:55 +0700

    crystal lattice r3

--- crystal_lattice.py
+++ crystal_lattice.py
@@ -1,27 +1,16 @@
-def check(row):
-    for i in range(1, len(row)):
-        if row[i] == row[i-1]:
-            return True
-    return False
-
-def golf(cube):
-    x = [check(row) for grid in cube for row in grid]
-    x += [check(row) for grid in cube for row in zip(*grid)]
-    x += [check(row) for grid in zip(*cube) for row in zip(*grid)]
-    return not any(x)
+def c(r):
+ for i in range(1,len(r)):
+  if r[i]==r[i-1]:return True
+ return False
+golf=lambda x:not any([c(r)for g in x+list(zip(*x))for r in g]+[c(r)for g in zip(*x)for r in zip(*g)])
 
 if __name__ == "__main__":
-    assert golf([[["a", "b"],
-                  ["c", "d"]],
-                 [["e", "f"],
-                  ["g", "h"]]]) == True, "st example"
- 
-    assert golf([[["X", "Z"],
+ assert golf([[["X", "Z"],
                   ["Z", "X"]],
                  [["Z", "X"],
                   ["X", "Z"]]]) == True, "1st example"
-    assert golf([[["X", "Z"],
+ assert golf([[["X", "Z"],
                   ["Z", "X"]],
                  [["X", "Z"],
                   ["Z", "X"]]]) == False, "2nd example"
-    print("All done? Earn rewards by using the 'Check' button!")
+ print("All done? Earn rewards by using the 'Check' button!")
