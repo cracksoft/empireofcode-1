d8a0307e096eeb6ad51ed2dc982cc30776e34c40
rawgni <ingwar.wirjawan@gmail.com>
2016-11-24 22:03:27 +0700

    transpose matrix js

---
+++ transpose_matrix.js
@@ -0,0 +1,16 @@
+"use strict";
+
+function transpose(matrix) {
+  return matrix[0].map((_, c) => matrix.map(r => r[c]))
+}
+
+var assert = require('assert');
+
+if (!global.is_checking) {
+  // These "asserts" using only for self-checking and not necessary for auto-testing
+  assert.deepEqual(transpose([[1, 2, 3], [4, 5, 6], [7, 8, 9]]),
+    [[1, 4, 7], [2, 5, 8], [3, 6, 9]], "Square matrix");
+  assert.deepEqual(transpose([[1, 4, 3], [8, 2, 6], [7, 8, 3], [4, 9, 6], [7, 8, 1]]),
+    [[1, 8, 7, 4, 7], [4, 2, 8, 9, 8], [3, 6, 3, 6, 1]], "Rectangle matrix");
+  console.log("All set? Click 'Check' to review your code and earn rewards!");
+}
