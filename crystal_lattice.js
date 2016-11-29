b04de75394932cec45ec05e348e339e4ce3a27a5
rawgni <ingwar.wirjawan@gmail.com>
2016-11-29 20:47:46 +0700

    crystal lattice js r1

---
+++ crystal_lattice.js
@@ -0,0 +1,37 @@
+function f(x){
+  var z=0;
+  for(var i in x){
+    if(z === x[i]) return 1
+    z = x[i]
+  }
+  return 0
+}
+function golf(g){
+  var m = g.map((_,c)=>g.map(r=>r[c]))
+  for (var i in g) if(f(g[i])) return 0
+  for (var i in m) if(f(m[i])) return 0
+  for (var i in g) {
+    var x = g[i].map((_,c)=>g[i].map(r=>r[c]))
+    for (var j in x) if(f(x[j])) return 0
+  }
+  for (var i in m) {
+    var x = m[i].map((_,c)=>m[i].map(r=>r[c]))
+    for (var j in x) if(f(x[j])) return 0
+  }
+  return 1
+}
+
+var assert = require('assert');
+
+if (!global.is_checking) {
+  // These "asserts" using only for self-checking and not necessary for auto-testing
+  assert.equal(golf([[["X", "Z"],
+    ["Z", "X"]],
+    [["Z", "X"],
+      ["X", "Z"]]]), true, "1st example)");
+  assert.equal(golf([[["X", "Z"],
+    ["Z", "X"]],
+    [["X", "Z"],
+      ["Z", "X"]]]), false, "2nd example)");
+  console.log("Now that you're finished, hit the 'Check' button to review your code and earn sweet rewards!");
+}
